177abf4ce1c1e75e6f6e18811e5c4824022899a9
kongbo <kongbo5582@163.com>
2022-06-30 19:04:54 +0800

    fix: 更换前端打包资源

---
+++ app/public/assets/index.1cf08612.js
@@ -0,0 +1,1 @@
+import{c as r}from"./index.ea975451.js";var d=()=>r("div",{children:"404"});export{d as default};
--- app/public/assets/index.2351e2c4.js
+++
@@ -1,1 +0,0 @@
-import{c as r}from"./index.2c3d9a54.js";var d=()=>r("div",{children:"404"});export{d as default};
